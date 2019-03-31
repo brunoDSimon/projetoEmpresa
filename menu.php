d3837b8f490410f12632472ef4eececd26465a3d
brunoDSimon <brunosimon999@gmail.com>
2019-03-31 12:38:05 -0300

    ajuste menu

--- menu.php
+++ menu.php
@@ -5,9 +5,16 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="stylesheet" type="text/css" href="css/reset.css">
+   <link rel="stylesheet" href="css/menu.css">
    
    <link rel="stylesheet" href="css/bootstrap.min.css">
    <title>MENU ADM</title>
+   <?php 
+    require_once('incs/conexao.php');
+
+    $sql = "SELECT sum(valor* quantidade) as total FROM modelo ";
+    $resultado = mysqli_query($conexao, $sql);
+   ?>
 </head>
     <body>
         
@@ -53,13 +60,138 @@
             </div>
         </nav>
         
-        <div class="bordas">
-            <div class="ganhos" >GANHOS</div>
-        </div>
-
-        <div class="bordas">
-            <div class="ganhos" >GASTOS</div>
-        </div>
+        <div class="box">
+    <div class="container">
+     	<div class="row">
+			 
+			    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div class="box-part text-center border">
+                        
+                        <i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
+                        
+						<div class="title">
+							<h2>Ganhos</h2>
+						</div>
+                        
+						<div class="text">
+                        <?php
+			$total = 0;
+			$qtde = 0;
+			while ($modelo = mysqli_fetch_array($resultado)){?>
+				
+					
+					
+					<?php $total +=  $modelo ['total']; $qtde += $modelo ['total']; ?>
+                    
+                    <h3>Total R$ <?php echo number_format($total,2,",",".");?></h3>
+			<?php   
+			} ?>	
+						</div>
+                        
+						
+                        
+					 </div>
+				</div>	 
+				
+				 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div class="box-part text-center border">
+					    
+					    <i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
+                    
+						<div class="title">
+							<h4>Twitter</h4>
+						</div>
+                        
+						<div class="text">
+							<span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+						</div>
+                        
+						<a href="#">Learn More</a>
+                        
+					 </div>
+				</div>	 
+				
+				 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div class="box-part text-center border">
+                        
+                        <i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
+                        
+						<div class="title">
+							<h4>Facebook</h4>
+						</div>
+                        
+						<div class="text">
+							<span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+						</div>
+                        
+						<a href="#">Learn More</a>
+                        
+					 </div>
+				</div>	 
+				
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div class="box-part text-center border">
+                        
+                        <i class="fa fa-pinterest-p fa-3x" aria-hidden="true"></i>
+                        
+						<div class="title">
+							<h4>Pinterest</h4>
+						</div>
+                        
+						<div class="text">
+							<span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+						</div>
+                        
+						<a href="#">Learn More</a>
+                        
+					 </div>
+				</div>	 
+				
+				 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div class="box-part text-center border">
+					    
+					    <i class="fa fa-google-plus fa-3x" aria-hidden="true"></i>
+                    
+						<div class="title">
+							<h4>Google</h4>
+						</div>
+                        
+						<div class="text">
+							<span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+						</div>
+                        
+						<a href="#">Learn More</a>
+                        
+					 </div>
+				</div>	 
+				
+				 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div class="box-part text-center border">
+                        
+                        <i class="fa fa-github fa-3x" aria-hidden="true"></i>
+                        
+						<div class="title">
+							<h4>Github</h4>
+						</div>
+                        
+						<div class="text">
+							<span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+						</div>
+                        
+						<a href="#">Learn More</a>
+                        
+					 </div>
+				</div>
+		
+		</div>		
+    </div>
+</div>
         
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
